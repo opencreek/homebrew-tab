3a7be472bddc4904937f38e2d4676922333a7c83
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2021-08-20 16:36:49 +0200

    :sparkles: Add creekey formular

---
+++ creekey.rb
@@ -0,0 +1,52 @@
+class Creekey < Formula
+  desc "Keep your private keys on your phone!"
+  homepage "https://creekey.io"
+  version "0.1.0"
+  license ""
+  url "https://github.com/opencreek/creekey-cli.git",:using => :git, tag: "main"
+
+  head "https://github.com:opencreek/creekey-cli.git"
+
+  depends_on "rust" => ":build"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    bin.install "target/release/creekey"
+    bin.install "target/release/creekey-git-sign"
+
+  end
+
+  plist_options :startup => true
+  def caveats
+    "Run 'creekey pair' to pair with your phone!"
+  end
+
+  def plist; <<-EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+        <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{bin}/creekey</string>
+        <string>agent</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <true/>
+      <key>StandardErrorPath</key>
+      <string>/dev/null</string>
+      <key>StandardOutPath</key>
+      <string>/dev/null</string>
+    </plist>
+  EOS
+  end
+
+  test do
+    system "which", "creekey"
+  end
+end
